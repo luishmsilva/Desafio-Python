50d6d9107454516dfdd6ab39a5558deeaaa9a8c7
luishmsilva <81598965+luishmsilva@users.noreply.github.com>
2022-03-25 09:32:17 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,16 @@
 import pandas as pd
-tabela = pd.read_csv("tabela_condicao.csv")
+import sqlite3 as sq
+
+banco = sq.connect('loja.db')
+cursor = banco.cursor()
+clientes = pd.read_sql('SELECT * FROM cliente', con=banco)
+clientes
+
+tabela = pd.read_csv('tabela_condicao.csv')
 print(tabela)
+
+pd.concat([clientes, tabela])
+left = clientes.set_index(['condicao'])
+right = tabela.set_index(['condicao'])
+left.join(right, lsuffix='_con')
 
